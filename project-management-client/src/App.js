e8bc93b9e8838762d3a78bcf36bd02892b5c1e2b
Aaron Carlson <acarlson90@gmail.com>
2018-12-26 08:22:57 -0800

    Include Bootstrap

--- project-management-client/src/App.js
+++ project-management-client/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header/>
+        <Header/>
         <Dashboard/>
       </div>
     );
