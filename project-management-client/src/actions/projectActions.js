30daa27e96c15069c8fb994005b9a8d3c4947101
Aaron Carlson <acarlson90@gmail.com>
2019-01-21 15:51:09 -0800

    Clear State for Errors on Successful POST

--- project-management-client/src/actions/projectActions.js
+++ project-management-client/src/actions/projectActions.js
@@ -5,6 +5,10 @@ export const createProject = (project, history) => async dispatch => {
     try {
         const response = await axios.post("http://localhost:8080/api/projects", project)
         history.push("/dashboard")
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
     } catch (error) {
         dispatch({
             type: GET_ERRORS,
